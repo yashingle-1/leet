856dbffc00025ec1fbe32799b60ba3d017c2573a
yashingle <yashinglelive@gmail.com>
2026-01-29 12:26:09 +0000

    Modified P2

---
+++ P2/add2num.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            
+            total = val1 + val2 + carry
+            carry = total // 10
+            
+            curr.next = ListNode(total % 10)
+            curr = curr.next
+            
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        
+        return dummy.next
